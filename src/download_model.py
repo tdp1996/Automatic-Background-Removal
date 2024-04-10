54e8ef18bdd8208af0c5fa2314d55c3e75764a6e
tdp1996 <phuongtd1107@gmail.com>
2024-04-10 16:41:09 +0700

    refactor download_model and test_download_model

--- src/download_model.py
+++ src/download_model.py
@@ -1,19 +1,23 @@
-import requests
 import os
+import gdown
+from pathlib import Path
 
-def download_model(s3_object_url, model_folder):
-    # Create the target directory if it doesn't exist
+def download_model(model_folder: Path) ->Path:
+    """
+    Download a model from Google Drive and save it to the specified folder.
+
+    Args:
+    - model_folder (Path): Path to the directory where the model will be saved.
+
+    Returns:
+    - Path: Path to the downloaded model file.
+    """
     if not os.path.exists(model_folder):
         os.makedirs(model_folder)
-    s3_object_response = requests.get(s3_object_url)
+    url = 'https://drive.google.com/uc?id=1vDmBJWMoWjhP0LyTZ-b8eXqwM9EohKHI'
+    output = 'ABR.onnx'
+    model_path = os.path.join(model_folder, output)
+    if not Path(model_path).exists():
+        gdown.download(url, model_path, quiet=False)
 
-    if s3_object_response.status_code == 200:
-        file_name = s3_object_url.split("/")[-1]
-        model_path = os.path.join(model_folder, file_name)
-        if not os.path.isfile(model_path):
-            # save the model
-            with open(model_path, 'wb') as local_file:
-                local_file.write(s3_object_response.content)
-    else:
-        print(f"Download failed. Status code: {s3_object_response.status_code}")
     return model_path
--- tests/test_download_model.py
+++ tests/test_download_model.py
@@ -1,11 +1,9 @@
-from download_model import download_model
 import os
 import tempfile
+from src.download_model import download_model
 
 def test_download_model():
-    with tempfile.TemporaryDirectory() as temp_model_folder:
-        s3_model_url = 'https://tdp-model.s3.ap-southeast-2.amazonaws.com/ABR_model/ABR_version_1.onnx'
-        model_path = download_model(s3_model_url, temp_model_folder)
-        assert os.path.exists(model_path)
+    with tempfile.TemporaryDirectory() as temp_model_folder:     
+        model_path = download_model(temp_model_folder)
         assert model_path.startswith(temp_model_folder)
-        assert model_path.endswith('.onnx')
+        assert model_path.endswith('ABR.onnx')
